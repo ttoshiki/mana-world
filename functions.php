813128013db3f0a07a5ab588c1261dd318ef3831
t-tamaru <t-tamaru@sakura.ad.jp>
2020-11-23 11:37:46 +0900

    svgをアップロード可能に

--- functions.php
+++ functions.php
@@ -1,7 +1,15 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' ); 
-function theme_enqueue_styles() { 
-	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-} 
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+function theme_enqueue_styles()
+{
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+}
 
+// ロゴにsvgを設定可能にする
+function custom_mime_types($mimes)
+{
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+}
+add_filter('upload_mimes', 'custom_mime_types');
