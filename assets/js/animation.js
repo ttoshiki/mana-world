e21708659196c402ff2de0f7797e7d741534b030
t-tamaru <t-tamaru@sakura.ad.jp>
2020-11-26 22:59:37 +0900

    スマホアニメーション実装

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,20 +1,38 @@
 "use strict";
 jQuery(function() {
   jQuery(window).on('load', function() {
-    jQuery('.animation__inner').append('<video src="/wp-content/themes/jin-child/assets/images/home/main.mp4" autoplay muted>')
+    if (window.matchMedia( "(min-width: 480px)" ).matches) {
+      jQuery('.animation__inner').append('<video src="/wp-content/themes/jin-child/assets/images/home/main.mp4" autoplay muted>')
 
-    setTimeout(() => {
-      jQuery('.animation__inner').append('<button class="animation__fadeout"></button>')
-      jQuery('.animation__fadeout').on('click', function() {
-        jQuery('.animation__wrapper').fadeOut(600)
-        setTimeout(() => {
-          jQuery('.animation__wrapper').remove()
-        }, 600);
-      })
-    }, 8200);
+      setTimeout(() => {
+        jQuery('.animation__inner').append('<button class="animation__fadeout"></button>')
+        jQuery('.animation__fadeout').on('click', function() {
+          jQuery('.animation__wrapper').fadeOut(600)
+          setTimeout(() => {
+            jQuery('.animation__wrapper').remove()
+          }, 600);
+        })
+      }, 8200);
 
-    setTimeout(() => {
-      jQuery('.animation__inner').append('<video src="/wp-content/themes/jin-child/assets/images/home/loop.mp4" id="loop-animation" autoplay muted loop>')
-    }, 8500);
+      setTimeout(() => {
+        jQuery('.animation__inner').append('<video src="/wp-content/themes/jin-child/assets/images/home/loop.mp4" id="loop-animation" autoplay muted loop>')
+      }, 8500);
+    } else {
+      jQuery('.animation__inner').append('<video src="/wp-content/themes/jin-child/assets/images/home/main-sp.mp4" autoplay muted>')
+
+      setTimeout(() => {
+        jQuery('.animation__inner').append('<button class="animation__fadeout"></button>')
+        jQuery('.animation__fadeout').on('click', function() {
+          jQuery('.animation__wrapper').fadeOut(600)
+          setTimeout(() => {
+            jQuery('.animation__wrapper').remove()
+          }, 600);
+        })
+      }, 8200);
+
+      setTimeout(() => {
+        jQuery('.animation__inner').append('<video src="/wp-content/themes/jin-child/assets/images/home/loop-sp.mp4" id="loop-animation" autoplay muted loop>')
+      }, 8500);
+    }
   })
 })
