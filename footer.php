f4f7b46a7c30dba1651592051fbc0438963a6d54
t-tamaru <t-tamaru@sakura.ad.jp>
2020-12-12 21:55:32 +0900

    contentの背景修正、spメニュー背景に透過をつける

--- footer.php
+++ footer.php
@@ -11,23 +11,23 @@
 
 ?>
 
-	<footer class="footer">
+  <footer class="footer">
     <div class="footer__toTop">
       <a href="#"><?php echo file_get_contents(get_stylesheet_directory_uri() . '/assets/images/icons/toTop.svg'); ?></a>
     </div>
-		<nav class="footer__nav">
-			<?php
+    <nav class="footer__nav">
+      <?php
         wp_nav_menu(array(
           'theme_location' => 'footer',
           'container_class' => 'menu-footer-container',
         ));
       ?>
-		</nav>
-		<div class="footer__companyCopyrightWrapper">
+    </nav>
+    <div class="footer__companyCopyrightWrapper">
       <p>掲載されているすべてのコンテンツ<br class="-sp">(記事、画像、音声データ、映像データ等)の無断転載を禁じます。</p>
-			<small class="footer__companyCopyright">© 2020 株式会社EXSENSE</small>
-		</div>
-	</footer>
+      <small class="footer__companyCopyright">© 2020 株式会社EXSENSE</small>
+    </div>
+  </footer>
 </div><!-- #wrapper -->
 
 <?php wp_footer(); ?>
